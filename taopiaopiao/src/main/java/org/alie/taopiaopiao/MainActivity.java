f47bd6f3cb70161558998c8d54696d711266c7e7
AlieYangpai <yangpai_beibei@163.com>
2019-08-25 16:10:47 +0800

    adjust plugin click event

--- taopiaopiao/src/main/java/org/alie/taopiaopiao/MainActivity.java
+++ taopiaopiao/src/main/java/org/alie/taopiaopiao/MainActivity.java
@@ -3,25 +3,28 @@ package org.alie.taopiaopiao;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 /**
  * 这里是不能是用插件本身的上下文的，必须使用宿主module的上下文才可以
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
-
+    private ImageView iv;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.iv).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(that,"插件apk界面点击",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(that, SecondActivity.class));
-            }
-        });
+        initView();
+        initListener();
+    }
+
+    private void initView(){
+        iv = (ImageView) findViewById(R.id.iv);
+    }
+    private void initListener(){
+        iv.setOnClickListener(this);
     }
 
     @Override
@@ -30,4 +33,13 @@ public class MainActivity extends BaseActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv:
+                Toast.makeText(that,"插件apk界面点击",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(that, SecondActivity.class));
+                break;
+        }
+    }
 }
